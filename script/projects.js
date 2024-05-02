7a56d51c1bf7732c9e95121c21552febd9420434
teenage-riot <kublahanych@gmail.com>
2024-05-02 18:04:41 +0300

    project buttons change color script

--- script/projects.js
+++ script/projects.js
@@ -1,23 +1,51 @@
-document.getElementById('all-btn').addEventListener('click', function() {
-  document.getElementById('web-design-img').style.display = 'flex';
-  document.getElementById('ui-img').style.display = 'flex';
-  document.getElementById('ux-img').style.display = 'flex';
+function setDisplayStyle(webDesign, ui, ux) {
+  document.getElementById('web-design-img').style.display = webDesign ? 'flex' : 'none';
+  document.getElementById('ui-img').style.display = ui ? 'flex' : 'none';
+  document.getElementById('ux-img').style.display = ux ? 'flex' : 'none';
+}
+
+function setButtonStyleActive(button) {
+  if (button == 'all') {
+    document.getElementById('all-btn').classList.add('colored-button')
+  } else {
+    document.getElementById('all-btn').classList.remove('colored-button');
+  }
+
+  if (button == 'ui') {
+    document.getElementById('ui-btn').classList.add('colored-button')
+  } else {
+    document.getElementById('ui-btn').classList.remove('colored-button');
+  }
+
+  if (button == 'ux') {
+    document.getElementById('ux-btn').classList.add('colored-button')
+  } else {
+    document.getElementById('ux-btn').classList.remove('colored-button');
+  }
+
+  if (button == 'web') {
+    document.getElementById('web-design-btn').classList.add('colored-button')
+  } else {
+    document.getElementById('web-design-btn').classList.remove('colored-button');
+  }
+}
+
+document.getElementById('all-btn').addEventListener('click', function () {
+  setDisplayStyle(true, true, true);
+  setButtonStyleActive('all');
 });
 
-document.getElementById('web-design-btn').addEventListener('click', function() {
-  document.getElementById('web-design-img').style.display = 'flex';
-  document.getElementById('ui-img').style.display = 'none';
-  document.getElementById('ux-img').style.display = 'none';
+document.getElementById('ui-btn').addEventListener('click', function () {
+  setDisplayStyle(false, true, false);
+  setButtonStyleActive('ui');
 });
 
-document.getElementById('ui-img-btn').addEventListener('click', function() {
-  document.getElementById('web-design-img').style.display = 'none';
-  document.getElementById('ui-img').style.display = 'flex';
-  document.getElementById('ux-img').style.display = 'none';
+document.getElementById('ux-btn').addEventListener('click', function () {
+  setDisplayStyle(false, false, true);
+  setButtonStyleActive('ux');
 });
 
-document.getElementById('ux-img-btn').addEventListener('click', function() {
-  document.getElementById('web-design-img').style.display = 'none';
-  document.getElementById('ui-img').style.display = 'none';
-  document.getElementById('ux-img').style.display = 'flex';
+document.getElementById('web-design-btn').addEventListener('click', function () {
+  setDisplayStyle(true, false, false);
+  setButtonStyleActive('web');
 });
